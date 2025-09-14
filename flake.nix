1a5942c38c007dbb348fdf564d800e04fc7462e8
Oskar Rochowiak <os1@qed.ai>
2025-09-14 22:07:03 +0200

    use tooling flake repo
    Change-Id: I6a6a696484db84412b7aa361b8b2fc9fcb550e90

--- flake.nix
+++ flake.nix
@@ -1,56 +1,15 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs.tooling.url = "github:mox-desktop/tooling";
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      rust-overlay,
-      ...
-    }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      overlays = [
-        (import rust-overlay)
-        (self: super: {
-          rustToolchain = super.rust-bin.selectLatestNightlyWith (
-            toolchain:
-            toolchain.default.override {
-              extensions = [
-                "rustc-codegen-cranelift-preview"
-                "rust-src"
-                "rustfmt"
-              ];
-            }
-          );
-        })
-      ];
-
-      forAllSystems =
-        function:
-        nixpkgs.lib.genAttrs systems (
-          system:
-          let
-            pkgs = import nixpkgs { inherit system overlays; };
-          in
-          function pkgs
-        );
-    in
-    {
-      devShells = forAllSystems (pkgs: {
+    { self, tooling, ... }:
+    tooling.lib.mkMoxFlake {
+      devShells = tooling.lib.forAllSystems (pkgs: {
         default = pkgs.mkShell (
           pkgs.lib.fix (finalAttrs: {
             buildInputs = builtins.attrValues {
               inherit (pkgs)
+                rustToolchain
                 rust-analyzer-unwrapped
                 nixd
                 vulkan-loader
@@ -69,32 +28,9 @@
             RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
           })
         );
-
       });
 
-      formatter = forAllSystems (
-        pkgs:
-        pkgs.writeShellApplication {
-          name = "nix3-fmt-wrapper";
-
-          runtimeInputs = builtins.attrValues {
-            inherit (pkgs)
-              rustToolchain
-              nixfmt-rfc-style
-              taplo
-              fd
-              ;
-          };
-
-          text = ''
-            fd "$@" -t f -e nix -x nixfmt -q '{}'
-            fd "$@" -t f -e toml -x taplo format '{}'
-            cargo fmt
-          '';
-        }
-      );
-
-      packages = forAllSystems (pkgs: {
+      packages = tooling.lib.forAllSystems (pkgs: {
         moxpaper = pkgs.callPackage ./nix/package.nix {
           rustPlatform = pkgs.makeRustPlatform {
             cargo = pkgs.rustToolchain;
